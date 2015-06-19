fb4f973071d36faa3260f84f8796ccd6a6950021
Eric <cpcheatsnerak2007@gmail.com>
2015-06-19 13:26:26 -0400

    Finished random generation!!!! WOOO!!!!

--- Assets/Scripts/Random/Level.cs
+++ Assets/Scripts/Random/Level.cs
@@ -3,12 +3,56 @@ using System.Collections;
 
 public class Level : MonoBehaviour {
 
+	public bool isTutorial;
+
+	Transform topEntrance;
+	Transform topExit;
+	
+	public LevelExit levelExit;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+		foreach(Transform child in transform) {
+			foreach(Transform childTwo in child) {
+				if(childTwo.tag == "TopEntrance" && !isTutorial)
+					this.topEntrance = childTwo;
+				if(childTwo.tag == "TopExit")
+					this.topExit = childTwo;
+			}
+		}
+		if(this.topEntrance == null && !isTutorial)
+			Debug.LogWarning("Level "+this.name+" has no entrance. Be sure to set the TopEntrance tag!");
+		if(this.topExit == null)
+			Debug.LogWarning("Level "+this.name+" has no exit. Be sure to set the TopExit tag!");
+	
+		//if(!isTutorial)
+		//	Debug.Log ("Level "+this.name+" Entrance:"+this.topEntrance.position+"/"+this.topEntrance.localPosition);
+		//Debug.Log ("Level "+this.name+" Exit:"+this.topExit.position+"/"+this.topExit.localPosition);
+		Debug.Log ("Level "+this.name);
+		if(levelExit == null)
+			Debug.Log("No exit! " +this.name);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	}
+
+	public Vector2 GetEntrancePosition() {
+		if(topEntrance == null)
+			Debug.LogWarning("Entrance doesn't exist!");
+		return this.topEntrance.position;
+	}
+
+	public Vector2 GetExitPosition() {
+		if(topExit == null)
+			Debug.LogWarning("Exit doesn't exist!");
+		return this.topExit.position;
+	}
+
+	public static Vector3 GetPositionOffset(Vector2 vectorOne, Vector2 vectorTwo) {
+		Vector2 v = vectorOne - vectorTwo;
+		return new Vector3(v.x, v.y, 0);
 	}
 
 	public GameObject GetLevelObject() {
@@ -16,6 +60,8 @@ public class Level : MonoBehaviour {
 	}
 
 	public void DestroyLevel() {
-		Destroy(this);
+		Debug.Log ("Destroy!");
+		Destroy(levelExit.gameObject);
+		Destroy(this.gameObject);
 	}
 }
--- Assets/Scripts/Random/LevelManager.cs
+++ Assets/Scripts/Random/LevelManager.cs
@@ -6,6 +6,7 @@ public class LevelManager : MonoBehaviour {
 	//Instance
 	public Level tutorialLevel;	
 	//Prefabs
+	public GameObject blockPrefab;
 	public Level[] levelList;
 
 	//0-1-2-3-4
@@ -14,31 +15,53 @@ public class LevelManager : MonoBehaviour {
 	//2 is the current room
 	[HideInInspector]
 	public Level[] levelQueue;
+
+	public static LevelManager GetLevelManager() {
+		return GameObject.FindGameObjectWithTag("LevelManager").GetComponent<LevelManager>();
+	}	
 	
 	// Use this for initialization
 	void Start () {
+		this.tag = "LevelManager";
 		levelQueue = new Level[5];
 		//Load first 3 levels.
 		levelQueue[2] = tutorialLevel.GetComponent<Level>();
 		levelQueue[3] = AddLevel(GetRandomPrefabIndex());
+		PositionLevel(levelQueue[2], levelQueue[3]);
 		levelQueue[4] = AddLevel(GetRandomPrefabIndex());
+		PositionLevel(levelQueue[3], levelQueue[4]);
 	}
 	
+	int lastIndex;
 	int GetRandomPrefabIndex() {
-		return Random.Range(0, levelList.Length);
+		int x = Random.Range(0, (levelList.Length));
+		if(x != lastIndex) {
+			lastIndex = x;
+			return x;
+		} else {
+			return GetRandomPrefabIndex();
+		}
 	}
 
 	//Loads 
 	Level AddLevel(int prefabIndex) {
 		return Instantiate(levelList[prefabIndex].gameObject).GetComponent<Level>();
-
 	}
 
 	void PositionLevel(Level anchorLevel, Level movingLevel) {
-
+		movingLevel.transform.position += Level.GetPositionOffset(anchorLevel.GetExitPosition(), movingLevel.GetEntrancePosition());
+		movingLevel.transform.position += Vector3.left;
 	}
 
-	void Update () {
-		
+	public void MoveQueueUp () {
+		if(levelQueue[0] != null)
+			levelQueue[0].DestroyLevel();
+
+		levelQueue[0] = levelQueue[1];
+		levelQueue[1] = levelQueue[2];
+		levelQueue[2] = levelQueue[3];
+		levelQueue[3] = levelQueue[4];
+		levelQueue[4] = AddLevel(GetRandomPrefabIndex());
+		PositionLevel(levelQueue[3], levelQueue[4]);
 	}
 }

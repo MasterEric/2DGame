b00a3d5b9e8e38b9e0cc9961d2e4fb668e824cf0
Toxoid_49b <Toxoid49b@gmail.com>
2015-07-15 21:02:34 -0400

    Umm...

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public AudioClip deathSound;
+
+	public Powerup.Powerups[] currentPowerups = new Powerup.Powerups[4];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Die(){
+
+		this.gameObject.SetActive(false);
+
+	}
+
+	public void AddPowerup(Powerup.Powerups powerupToAdd){
+
+		Debug.Log ("Collected powerup of type " + powerupToAdd.ToString() + ".");
+
+	}
+
+	public bool HasPowerup(Powerup.Powerups powerupToCheck){
+
+		foreach(Powerup.Powerups p in currentPowerups){
+
+			if(p == powerupToCheck){
+
+				return true;
+
+			} else {
+
+				return false;
+
+			}
+		}
+
+		return false;
+
+	}
+}
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+	public ParticleSystem playerDeathSpikeParticles;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if(other.GetComponent<Player>() != null){
+		
+			other.GetComponent<Player>().Die();			
+			Instantiate(playerDeathSpikeParticles, other.transform.position, Quaternion.identity);
+
+
+		}
+	}
+}
